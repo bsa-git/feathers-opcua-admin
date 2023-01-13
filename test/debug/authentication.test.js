b7c026ea26b92a63edad33f5e2fe05e016689a41
Sergey Beskorovainy <bs261257@gmail.com>
2023-01-13 15:54:03 +0200

    Update

--- test/debug/authentication.test.js
+++ test/debug/authentication.test.js
@@ -106,47 +106,52 @@ describe(`<<<=== Test "${__filename.substring(__dirname.length + 1)}" ===>>>`, (
         try {
           service = appClient.service('users');
           const user = await service.create(newUser);
-          if (true && user) logger.info(chalk.yellow(`${transport} appClient.create user - OK`));
+          if (true && user) logger.info(chalk.yellow(`"${transport}" appClient.create user - OK`));
           if (isDebug && user) inspector('service.create(newUser):', user);
           assert.ok(user, 'New use not created.');
         } catch (error) {
           await appClient.logout();
-          if (true && user) logger.info(chalk.yellow(`${transport} New user already exists!`));
+          if (true && user) logger.info(chalk.yellow(`"${transport}" New user already exists!`));
         }
 
       });
 
       it(`#2.${index + 1}: Test - For "${transport}" appClient login to newUser`, async () => {
-        // Login local for newUser.email, newUser.password
-        let { accessToken } = await loginLocal(appClient, newUser.email, newUser.password);
-        assert.ok(accessToken, 'Authentication accessToken');
-        if (accessToken) {
+        try {
+          // Login local for newUser.email, newUser.password
+          let { accessToken } = await loginLocal(appClient, newUser.email, newUser.password);
+          assert.ok(accessToken, 'Authentication accessToken');
           myAccessToken = accessToken;
           const payload = await AuthServer.verifyJWT(accessToken);
           if (isDebug && payload) inspector('AuthServer.verifyJWT.payload:', payload);
           userId = payload.userId ? payload.userId : payload.sub;
           if (isDebug && userId) inspector('newUser.userId:', userId);
           if (isDebug && accessToken) {
-            inspector(`${transport} appClient.passwort.getJWT:`, await AuthServer.getPassportJWT(appClient));
-            inspector(`${transport} appClient.passwort.verifyJWT:`, await AuthServer.verifyPassportJWT(appClient, accessToken));
+            inspector(`"${transport}" appClient.passwort.getJWT:`, await AuthServer.getPassportJWT(appClient));
+            inspector(`"${transport}" appClient.passwort.verifyJWT:`, await AuthServer.verifyPassportJWT(appClient, accessToken));
           }
-          logger.info(chalk.yellow(`${transport} appClient loginLocal - OK`));
+          logger.info(chalk.yellow(`"${transport}" appClient loginLocal - OK`));
+        } catch (error) {
+          if (true && error) logger.error(`Error for "${transport}" appClient login to newUser. Error.message: "${error.message}"`);
+          assert.ok(false, `Error for "${transport}" appClient login to newUser.`);
         }
       });
 
 
       it(`#3.${index + 1}: Test - For "${transport}" appClient get newUser`, async () => {
-        let service = appClient.service('users');
+        if(!myAccessToken) return;
+        const service = appClient.service('users');
         const user = await service.get(userId);
-        if (true && user) logger.info(chalk.yellow(`${transport} appClient get newUser for id: ("${user[idField]}") - OK`));
-        assert.ok(user[idField] === userId, `${transport} appClient dosn't get newUser`);
+        if (true && user) logger.info(chalk.yellow(`"${transport}" appClient get newUser for id: ("${user[idField]}") - OK`));
+        assert.ok(user[idField] === userId, `"${transport}" appClient dosn't get newUser`);
       });
 
       it(`#4.${index + 1}: Test - For "${transport}" appClient remove newUser`, async () => {
-        let service = appClient.service('users');
+        if(!myAccessToken) return;
+        const service = appClient.service('users');
         const user = await service.remove(userId);
-        if (true && user) logger.info(chalk.yellow(`${transport} appClient remove newUser for id: ("${userId}") - OK`));
-        assert.ok(user.email === newUser.email, `${transport} appClient dosn't remove lastUser`);
+        if (true && user) logger.info(chalk.yellow(`"${transport}" appClient remove newUser for id: ("${userId}") - OK`));
+        assert.ok(user.email === newUser.email, `"${transport}" appClient dosn't remove lastUser`);
       });
     });
   });
